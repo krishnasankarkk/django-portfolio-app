4dacbaa93e2e6c2268759ce7bd6533ef30c063fd
kichu <sankarkrishna689@gmail.com>
2023-09-28 13:42:08 +0530

    vercel

--- shoppingonline/settings.py
+++ shoppingonline/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wnms(ree0#_8+bvm(xaf=h*5grypp_=r@4$vct2_1sp)nq-n0l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
