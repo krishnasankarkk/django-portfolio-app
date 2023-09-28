d2fcf86df89f857c092849b710b05846c21ed2e1
kichu <sankarkrishna689@gmail.com>
2023-09-28 14:18:32 +0530

    latest changes

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,7 @@
 echo "BUILD START"
+python3 -m pip install virtualenv
+python3 -m venv env
+python3 -m env\scripts\activate
 python3 -m pip install -r requirements.txt
 python3 -m manage collectstatic --noinput --clear
 echo "BUILD END"
